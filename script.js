477f5782236ca89ca1745af822008e79b6e3d4db
unknown <sirbbia26@gmail.com>
2021-04-22 22:11:09 +0300

    Elemente noi

---
+++ script.js
@@ -0,0 +1,97 @@
+var xul;
+var zero;
+//Functia 1
+let scriex1 = function(choice){
+    document.getElementsByClassName("unu")[0].innerText =  "❌"
+    xul = choice;
+    
+}
+let scrie01 = function(choice){
+    document.getElementsByClassName("unu")[0].innerText =  " ⭕️ "
+    zero = choice;
+    
+}
+
+//Functia 2
+let scriex2 = function(choice){
+    document.getElementsByClassName("doi")[0].innerText =  "❌"
+    xul = choice;
+}
+let scrie02 = function(choice){
+    document.getElementsByClassName("doi")[0].innerText =  " ⭕️ "
+    zero = choice;
+}
+//Functia 3
+let scriex3 = function(choice){
+    document.getElementsByClassName("trei")[0].innerText =  "❌"
+    xul = choice;
+   
+    
+}
+let scrie03 = function(choice){
+    document.getElementsByClassName("trei")[0].innerText =  " ⭕️ "
+    zero = choice;
+}
+//Functia 4
+let scriex4 = function(choice){
+    document.getElementsByClassName("patru")[0].innerText =  "❌"
+    xul = choice;
+    
+}
+let scrie04 = function(choice){
+    document.getElementsByClassName("patru")[0].innerText =  "⭕️ "
+    zero = choice;
+}
+//Functia 5
+let scriex5 = function(choice){
+    document.getElementsByClassName("cinci")[0].innerText =  "❌"
+    xul = choice;
+    
+}
+let scrie05 = function(choice){
+    document.getElementsByClassName("cinci")[0].innerText =  "⭕️ "
+    zero = choice;
+}
+//Functia 6
+let scriex6 = function(choice){
+    document.getElementsByClassName("sase")[0].innerText =  "❌"
+    xul = choice;
+}
+let scrie06 = function(choice){
+    document.getElementsByClassName("sase")[0].innerText =  "⭕️ "
+    zero = choice;
+}
+//Functia 7
+let scriex7 = function(choice){
+    document.getElementsByClassName("sapte")[0].innerText =  "❌"
+    xul = choice;
+    
+}
+let scrie07 = function(choice){
+    document.getElementsByClassName("sapte")[0].innerText =  "⭕️ "
+    zero = choice;
+}
+//Functia 8
+let scriex8 = function(choice){
+    document.getElementsByClassName("opt")[0].innerText =  "❌"
+    xul = choice;
+    
+}
+let scrie08 = function(choice){
+    document.getElementsByClassName("opt")[0].innerText =  "⭕️ "
+    zero = choice;
+}
+//Functia 9
+let scriex9 = function(choice){
+    document.getElementsByClassName("noua")[0].innerText =  "❌"
+    xul = choice;
+    
+}
+let scrie09 = function(choice){
+    document.getElementsByClassName("noua")[0].innerText =  "⭕️ "
+    zero = choice;
+}
+//.........................................................................
+
+
+
