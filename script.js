6aa67ec45e8144601d431db07c423ae2046ca93e
unknown <sirbbia26@gmail.com>
2021-04-22 23:57:11 +0300

    /////

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var turn= "❌";
 //Functia 1
-let scriex1 = function(turn){
+let scriex1 = function(){
    
     if(turn === true){
         document.getElementsByClassName("unu")[0].innerText =  "❌"
@@ -14,7 +14,7 @@ let scriex1 = function(turn){
 
 
 //Functia 2
-let scriex2 = function(turn){
+let scriex2 = function(){
     if(turn === true){
         document.getElementsByClassName("doi")[0].innerText =  "⭕️"
         turn = false;
@@ -26,7 +26,7 @@ let scriex2 = function(turn){
 }
 
 //Functia 3
-let scriex3 = function(turn){
+let scriex3 = function(){
     if(turn === true){
         document.getElementsByClassName("trei")[0].innerText =  "❌"
         turn = false;
@@ -38,7 +38,7 @@ let scriex3 = function(turn){
 }
 
 //Functia 4
-let scriex4 = function(turn){
+let scriex4 = function(){
     if(turn === true){
         document.getElementsByClassName("patru")[0].innerText =  "⭕️"
         turn = false;
@@ -50,7 +50,7 @@ let scriex4 = function(turn){
 }
 
 //Functia 5
-let scriex5 = function(turn){
+let scriex5 = function(){
     if(turn === true){
         document.getElementsByClassName("cinci")[0].innerText =  "❌"
         turn = false;
@@ -62,7 +62,7 @@ let scriex5 = function(turn){
 }
 
 //Functia 6
-let scriex6 = function(turn){
+let scriex6 = function(){
     if(turn === false){
         document.getElementsByClassName("sase")[0].innerText =  "⭕️"
         turn = true;
@@ -74,7 +74,7 @@ let scriex6 = function(turn){
 }
 
 //Functia 7
-let scriex7 = function(turn){
+let scriex7 = function(){
     if(turn === true){
         document.getElementsByClassName("sapte")[0].innerText =  "❌"
         turn = false;
@@ -86,7 +86,7 @@ let scriex7 = function(turn){
 }
 
 //Functia 8
-let scriex8 = function(turn){
+let scriex8 = function(){
     if(turn === false){
         document.getElementsByClassName("opt")[0].innerText =  "⭕️"
         turn = true;
@@ -99,7 +99,7 @@ let scriex8 = function(turn){
 }
 
 //Functia 9
-let scriex9 = function(turn){
+let scriex9 = function(){
     if(turn === true){
         document.getElementsByClassName("noua")[0].innerText =  "❌"
         turn = false;
