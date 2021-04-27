113f7d37383d30953d9f5ccd864d926391bac035
unknown <sirbbia26@gmail.com>
2021-04-27 18:49:18 +0300

    Aproape gata

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 var turn= true;
+var pxChoice;
+var pzChoice;
 //Functia 1
-let scriex1 = function(){
-   
+let alege1 = function(){
     if(turn === true){
         document.getElementsByClassName("unu")[0].innerText =  "❌"
        turn = false;
@@ -9,12 +10,14 @@ let scriex1 = function(){
         document.getElementsByClassName("unu")[0].innerText =  "⭕️"
         turn = true;
     }
+    pxChoice = turn;
    
+    
 }
 
 
 //Functia 2
-let scriex2 = function(){
+let alege2 = function(){
     if(turn === false){
         document.getElementsByClassName("doi")[0].innerText =  "⭕️"
         turn = true;
@@ -22,11 +25,12 @@ let scriex2 = function(){
         document.getElementsByClassName("doi")[0].innerText =  "❌"
         turn = false;
     }
-    
+    pzChoice = turn;
+   
 }
 
 //Functia 3
-let scriex3 = function(){
+let alege3 = function(){
     if(turn === true){
         document.getElementsByClassName("trei")[0].innerText =  "❌"
         turn = false;
@@ -34,11 +38,12 @@ let scriex3 = function(){
         document.getElementsByClassName("trei")[0].innerText =  "⭕️"
         turn = true;
     }
-    
+    pxChoice = turn;
+   
 }
 
 //Functia 4
-let scriex4 = function(){
+let alege4 = function(){
     if(turn === false){
         document.getElementsByClassName("patru")[0].innerText =  "⭕️"
       turn = true;
@@ -46,11 +51,11 @@ let scriex4 = function(){
         document.getElementsByClassName("patru")[0].innerText =  "❌"
        turn = false;
     }
-    
+    pzChoice = turn;
 }
 
 //Functia 5
-let scriex5 = function(){
+let alege5 = function(){
     if(turn === true){
         document.getElementsByClassName("cinci")[0].innerText =  "❌"
        turn = false;
@@ -58,11 +63,11 @@ let scriex5 = function(){
         document.getElementsByClassName("cinci")[0].innerText =  "⭕️"
         turn = true;
     }
-    
+    pxChoice = turn;
 }
 
 //Functia 6
-let scriex6 = function(){
+let alege6 = function(){
     if(turn === false){
         document.getElementsByClassName("sase")[0].innerText =  "⭕️"
        turn = true;
@@ -71,10 +76,11 @@ let scriex6 = function(){
         document.getElementsByClassName("sase")[0].innerText =  "❌"
         turn = false;
     }
+    pzChoice = turn;
 }
 
 //Functia 7
-let scriex7 = function(){
+let alege7 = function(){
     if(turn === true){
         document.getElementsByClassName("sapte")[0].innerText =  "❌"
         turn = false;
@@ -82,11 +88,11 @@ let scriex7 = function(){
         document.getElementsByClassName("sapte")[0].innerText =  "⭕️"
       turn = true;
     }
-    
+    pxChoice = turn;
 }
 
 //Functia 8
-let scriex8 = function(){
+let alege8 = function(){
     if(turn === false){
         document.getElementsByClassName("opt")[0].innerText =  "⭕️"
         turn = true;
@@ -95,11 +101,11 @@ let scriex8 = function(){
         document.getElementsByClassName("opt")[0].innerText =  "❌"
        turn = false;
     }
-    
+    pzChoice = turn;
 }
 
 //Functia 9
-let scriex9 = function(){
+let alege9 = function(){
     if(turn === true){
         document.getElementsByClassName("noua")[0].innerText =  "❌"
         turn = false;
@@ -107,8 +113,10 @@ let scriex9 = function(){
         document.getElementsByClassName("noua")[0].innerText =  "⭕️"
         turn = true;
     }
-    document.getElementsByClassName("final")[0].innerText = "Joc terminat"   
+    pxChoice = turn;
+  
 }
+
 
 //.........................................................................
 
